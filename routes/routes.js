505e6e6fe8da12dc670a045db9c6124dcfb02da6
Anthony <anthonyfdez23@gmail.com>
2022-03-01 15:58:59 -0500

    auto update after 6 hours

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,34 @@ const express = require("express");
 
 const updateData = require("../updateData");
 const router = new express.Router();
+const fs = require("fs");
+
+router.get("/history", async (req, res) => {
+  try {
+    try {
+      const lastUpdated = require("../lastUpdated.json");
+      const dateNow = new Date().getTime() / 1000;
+      const difference = dateNow - lastUpdated.lastUpdated;
+
+      if (difference > 30) {
+        await updateData({ forceUpdate: true });
+      }
+    } catch {
+      await updateData({ forceUpdate: true });
+    }
+
+    const allDates = require("../allDates.json");
+
+    const paginated = allDates.table.slice(
+      req.body.start || 0,
+      req.body.end || 75
+    );
+
+    res.status(200).send({ table: paginated, total: sortedNumbers.length });
+  } catch (err) {
+    res.status(500).send("error");
+  }
+});
 
 router.get("/sorted/up", async (req, res) => {
   try {
@@ -9,22 +37,71 @@ router.get("/sorted/up", async (req, res) => {
 
     const sortedNumbers = require("../sortedNumbers.json");
 
-    res.status(200).send(sortedNumbers);
+    const paginated = sortedNumbers.table.slice(
+      req.body.start || 0,
+      req.body.end || 75
+    );
+
+    res.status(200).send({ table: paginated, total: sortedNumbers.length });
   } catch (err) {
     res.status(500).send("error");
   }
 });
 
-router.get("/sorted/oldest", async (req, res) => {
+router.get("/sorted/down", async (req, res) => {
   try {
     await updateData();
 
-    const sortedNumbersOldest = require("../sortedNumbers_oldest.json");
+    const sortedNumbers = require("../sortedNumbers.json");
 
-    res.status(200).send(sortedNumbersOldest);
+    const sortedDown = sortedNumbers.table.reverse();
+
+    const paginated = sortedDown.slice(req.body.start || 0, req.body.end || 75);
+
+    res.status(200).send({ table: paginated, total: sortedNumbers.length });
   } catch (err) {
     res.status(500).send("error");
   }
 });
+
+router.post("/sorted/oldest", async (req, res) => {
+  try {
+    const lastUpdated = returnlastUpdated();
+    const dateNow = new Date().getTime() / 1000;
+    const difference = dateNow - lastUpdated.lastUpdated;
+
+    // six hours
+    if (difference > 21600) {
+      await updateData({ forceUpdate: true });
+    } else {
+      await updateData({ forceUpdate: false });
+    }
+
+    const sortedNumbersOldest = JSON.parse(
+      fs.readFileSync("sortedNumbers_oldest.json", "utf-8")
+    );
+
+    const paginated = sortedNumbersOldest.table.slice(
+      req.body.start || 0,
+      req.body.end || 75
+    );
+
+    res
+      .status(200)
+      .send({ table: paginated, total: sortedNumbersOldest.length });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("error");
+  }
+});
+
+const returnlastUpdated = () => {
+  try {
+    let jsonData = JSON.parse(fs.readFileSync("lastUpdated.json", "utf-8"));
+    return jsonData;
+  } catch {
+    console.log("failed loading last upadted");
+  }
+};
 
 module.exports = router;
--- updateData.js
+++ updateData.js
@@ -1,13 +1,32 @@
+const fs = require("fs").promises;
+
 const loadData = require("./functions/loadData");
 const getStats = require("./functions/getStats");
 const getAverage = require("./functions/getAverage");
 const getMedian = require("./functions/getMedian");
 const getOldestNumbers = require("./functions/getOldestNumbers");
 
-const updateData = async () => {
-  const allDates = await loadData({ update: true });
-  const sortedNumbers = await getStats({ data: allDates });
-  await getOldestNumbers({ data: sortedNumbers });
+const updateData = async ({ forceUpdate }) => {
+  if (forceUpdate) {
+    await saveLastUpdated();
+
+    const allDates = await loadData({ update: true });
+    const sortedNumbers = await getStats({ data: allDates });
+    await getOldestNumbers({ data: sortedNumbers });
+  } else {
+    const allDates = await loadData({ update: false });
+    const sortedNumbers = await getStats({ data: allDates });
+    await getOldestNumbers({ data: sortedNumbers });
+  }
+};
+
+const saveLastUpdated = async () => {
+  const obj = {
+    lastUpdated: new Date().getTime() / 1000,
+  };
+
+  var json = JSON.stringify(obj);
+  await fs.writeFile("lastUpdated.json", json);
 };
 
 module.exports = updateData;
